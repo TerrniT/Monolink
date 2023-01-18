dc949fa569dfb3970de820cb51bf7c4954f90bf1
TerrniT <terrnit.megaman@gmail.com>
2023-01-18 16:45:06 +0500

    init(): configurate tailwind css/ supabase client

---
+++ pages/signup.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import Image from "next/image"
+
+const SignUp = () => {
+  const [email, setEmail] = useState<string | undefined>()
+  const [password, setPassword] = useState<string | undefined>()
+
+
+  return (
+
+    <div className="flex h-screen w-full items-center justify-center bg-zinc-800">
+      <div className="rounded-xl bg-black filter backdrop-filter bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-lg max-sm:px-8" >
+
+        <div className="text-white">
+          <div className="mb-8 flex flex-col items-center">
+            <div className='flex flex-row gap-2 items-center justify-center'>
+              <h1 className="mb-2 text-2xl">Monolink</h1>
+              <Image src="/monolink.svg" width={24} height={24} alt="Monolink logo" />
+            </div>
+            <span className="text-gray-300 mt-5">Enter Login Details</span>
+          </div>
+          <form action="#" className='flex flex-col'>
+            <div className="mb-4 text-lg">
+              <input className="rounded-3xl bg-black bg-opacity-50 border-none px-6 py-3 text-center text-inherit placeholder-gray-500 shadow-lg outline-none backdrop-blur-md" type="email" name="email" id='email' placeholder="enter your email" />
+            </div>
+
+            <div className="mb-4 text-lg">
+              <input className="rounded-3xl border-none bg-black bg-opacity-50 px-6 py-3 text-center text-inherit placeholder-gray-500 shadow-lg outline-none backdrop-blur-md" type="password" name="password" id='password' placeholder="password" />
+            </div>
+            <button type="button" className="mt-4 self-center border border-gray-400 rounded-3xl bg-black bg-opacity-50 px-10 py-2 text-white backdrop-blur-md transition-colors duration-300 hover:bg-zinc-800">Sign up</button>
+          </form>
+        </div>
+      </div >
+    </div >
+  )
+}
+
+export default SignUp 
