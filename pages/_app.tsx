e866935b713b0f32f22c08f8216de0cb0011e351
TerrniT <terrnit.megaman@gmail.com>
2023-01-19 22:35:24 +0500

    feat(home): create welcome page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+//ts-ignore
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
