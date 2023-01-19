ab1cfd319ff46bc6797a52147a839374c2278841
TerrniT <terrnit.megaman@gmail.com>
2023-01-19 22:35:50 +0500

    feat(button): create ui button

---
+++ components/atoms/button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface ButtonProps extends React.ComponentProps<"button"> {
+  label: string,
+}
+
+const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="text-white border border-slate-400 rounded-xl py-2 px-4 backdrop-blur-lg filter bg-transparent hover:border-zinc-600 hover:text-gray-800 transition-all duration-150 text-sm"
+    >
+      {label}
+    </button>
+  )
+}
+
+export default Button
