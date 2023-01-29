019617b94c1db3ba0718f6f0df9af28dc9bd1781
TerrniT <terrnit.megaman@gmail.com>
2023-01-29 20:30:20 +0500

    chore(*): delete un-used files

--- components/providers/authetication/UpdatePassword.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UpdatePassword = () => {
-  return <div>UpdatePassword</div>;
-};
-
-export default UpdatePassword;
--- pages/api/getUser.js
+++
@@ -1,8 +0,0 @@
-
-import { supabase } from '@/utils/supabaseClient'
-
-export default async function getUser(req, res) {
-  const user = await supabase.auth.getUser();
-  return res.status(200).json({ user: user });
-}
-
