a925685e7c38bdf92085f8559f02b8a06be055df
TerrniT <terrnit.megaman@gmail.com>
2023-04-15 21:14:47 +0500

    fix(deploy): deploy fix

---
+++ components/ui/skeleton/DashboardSkeleton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const DashboardSkeleton = (props: Props) => {
+  return (
+    <div>DashboardSkeleton</div>
+  )
+}
+
+export default DashboardSkeleton
