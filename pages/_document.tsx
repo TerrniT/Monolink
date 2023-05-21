b9569e30f68a1bd972363c109d09b746135a89c1
TerrniT <terrnit.megaman@gmail.com>
2023-05-21 19:14:27 +0500

    chore(seo\): meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,32 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+export default function Document({ pageMeta }: any) {
+
+  const meta = {
+    title: 'Monolink',
+    description: "Some description for SEO",
+    type: 'website',
+    ...pageMeta,
+  }
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>{meta.title}</title>
+        <meta name='description' content={meta.description} />
+        <link rel='icon' href='/monolink-rev.svg' />
+        <meta
+          property='og:url'
+          content={`https://monolink.vercel.app`}
+        />
+        <meta property='og:type' content={meta.type} />
+        <meta property='og:site_name' content='Monolink' />
+        <meta property='og:description' content={meta.description} />
+        <meta property='og:title' content={meta.title} />
+        {meta.data && (
+          <meta property='articel:published_time' content={meta.date} />
+        )}
+      </Head>
       <body>
         <Main />
         <NextScript />
